4779fedd2717a2dfd9192a4936003109ef916096
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-21 18:45:46 +0600

    transactions models done

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Book
 
-# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_date = models.DateTimeField(auto_now_add=True)
+    returned_date = models.DateTimeField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
