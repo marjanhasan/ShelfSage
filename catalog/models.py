85b01c5dd2a66290ad2ec1ff1dd5a4e91f0f856d
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-21 18:35:44 +0600

    catalog & wishlist models created successfully

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    isbn = models.CharField('ISBN', max_length=13, unique=True)
+    publication_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    availability_status = models.BooleanField(default=True)
+    number_of_copies = models.PositiveIntegerField(default=1)
+    image = models.ImageField(
+        upload_to="catalog/media/uploads",
+        height_field=None,
+        width_field=None,
+        max_length=None,
+        default="",
+    )
+    
+    def __str__(self):
+        return self.title
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Book
 
-# Create your models here.
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'book')
+        verbose_name_plural = 'wishlists'
+
+    def __str__(self):
+        return f"{self.user.username}'s wishlist"
