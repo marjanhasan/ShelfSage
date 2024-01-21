fdd136cfb5d8553b78ab885279aac05d685b90f8
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-21 18:57:01 +0600

    reviews model created successfully

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Book
+from django.core.validators import MaxValueValidator, MinValueValidator
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return f"Review by {self.user.username} on {self.book.title}"
