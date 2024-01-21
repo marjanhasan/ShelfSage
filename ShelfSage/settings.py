4a8c02088dc74531f788761478f7590f070a2500
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-22 02:59:50 +0600

    all design added

--- ShelfSage/settings.py
+++ ShelfSage/settings.py
@@ -30,8 +30,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ["*"]
+# CSRF_TRUSTED_ORIGINS = ['https://lms-e0eu.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
